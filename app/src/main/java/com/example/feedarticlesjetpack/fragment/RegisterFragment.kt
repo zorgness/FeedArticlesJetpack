fecc8abd52b9489051c7cc56b170888ba3f5c91f
Thomas Fouquert <tfouquert@gmail.com>
2023-05-03 12:25:57 +0200

    update on register fragment binding

--- app/src/main/java/com/example/feedarticlesjetpack/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/feedarticlesjetpack/fragment/RegisterFragment.kt
@@ -10,26 +10,29 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.feedarticlesjetpack.R
+import com.example.feedarticlesjetpack.databinding.FragmentLoginBinding
 import com.example.feedarticlesjetpack.databinding.FragmentRegisterBinding
 import com.example.feedarticlesjetpack.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import com.example.feedarticlesjetpack.extensions.myToast
 
 @AndroidEntryPoint
-class RegisterFragment: Fragment() {
+class RegisterFragment : Fragment() {
 
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
     private val myViewModel: RegisterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        myViewModel.messageLiveData.observe(this) {message ->
+        myViewModel.messageLiveData.observe(this) { message ->
             activity?.myToast(message)
         }
 
 
-        myViewModel.statusLiveData.observe(this) {status ->
-            if(status == STATUS_USER_SUCCESS) {
+        myViewModel.statusLiveData.observe(this) { status ->
+            (status == STATUS_USER_SUCCESS).run {
                 val navDir = RegisterFragmentDirections.actionRegisterFragmentToMainFragment()
                 findNavController().navigate(navDir)
             }
@@ -41,8 +44,8 @@ class RegisterFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        val binding: FragmentRegisterBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
 
         binding.btnRegister.setOnClickListener {
             myViewModel.register(
@@ -53,5 +56,10 @@ class RegisterFragment: Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
